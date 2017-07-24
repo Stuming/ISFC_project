9de087703173233015554dda9b8333b060e9a971
Stuming <1361046649@qq.com>
2017-07-24 20:37:08 +0800

    nothing new

--- scripts/main_analysis_group.py
+++ scripts/main_analysis_group.py
@@ -6,21 +6,18 @@ from ISFC_tools import cal_ISFC,cal_ISC,load_file,match_datashape,save_result
 
 # sessid="/s1/data/gumpdata/project/sessid"
 datadir="/s1/data/gumpdata/project"
-sessid1="S001"
-sessid2="S002"
 sessidlist=["S001", "S002", "S003", "S004", "S005", "S006", "S009", "S010", "S014", "S015", "S016", "S017", "S018", "S019", "S020"]
 funcname="bold"
 filename="fmcpr.up.sm0.fsaverage.lh.mgh" # Do analysis after mri_convert
 
-for r in range(1,2):
+for r in range(8,9):
     runid=str(r)
     while len(runid)<3:
         runid="0"+runid
 
-    for sessid_temp in sessidlist: # source session/subject
-        if sessid1 == sessid_temp:
-            continue
-
+    # for sessid_temp in sessidlist: # source session/subject
+    sessid_temp="S001"
+    if 1==1:
         avg_data_path="avg_data_except_"+sessid_temp+"_"+runid+".mgh"
         f1=load_file(datadir, sessid_temp, funcname, runid, filename)
         f2=nib.load(avg_data_path)
@@ -45,6 +42,6 @@ for r in range(1,2):
         print("-----")
 
         trg_sessid="group"
-        save_result(method_name,result_f,sessid,trg_sessid,runid,vertex_num)
+        save_result(method_name,result_f,sessid_temp,trg_sessid,runid,vertex_num)
 
 print("-----------End-----------")
