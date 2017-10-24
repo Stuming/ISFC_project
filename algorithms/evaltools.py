19adbfad49974d895ffbc8ea90552e7abe25208a
Stuming <1361046649@qq.com>
2017-10-24 15:13:33 +0800

    *NF*calculate homogeneity and dice

--- algorithms/evaltools.py
+++ algorithms/evaltools.py
@@ -3,7 +3,8 @@ Used to evaluate clusters or parcellations.
 """
 import numpy as np
 from .fctools import wsfc
-from sklearn.metrics.cluster import adjusted_rand_score, adjusted_mutual_info_score, silhouette_samples
+from ATT.algorithm.tools import calc_overlap
+from sklearn.metrics.cluster import adjusted_rand_score, adjusted_mutual_info_score, silhouette_score
 
 
 def ari(parcel1, parcel2):
@@ -56,13 +57,18 @@ def homogeneity(data, parcel):
         1. the max label number in parcel should be assigned to the medial wall.
     """
     max_label = np.max(parcel)
+    homo_list = np.zeros(max_label)
     for label in range(max_label):
-        wsfc(data[np.where(parcel == label)])
+        vert_list = np.where(parcel == label)
+        vert_num = vert_list.shape[0]
+        fcmap = wsfc(data[vert_list, :])
+        homo_list[label] = 2 * np.sum(fcmap - vert_num) / (vert_num * (vert_num - 1))
+    return np.mean(homo_list)
 
 
-def dice_coef(parcel1, parcel2):
+def dice_mat(parcel1, parcel2):
     """
-    Calculate dice coefficient of parcel1 and parcel2.
+    Calculate dice coefficient matrix of parcel1 and parcel2.
 
     Parameters
     ----------
@@ -71,22 +77,52 @@ def dice_coef(parcel1, parcel2):
 
     Returns
     -------
-        dice coef: ranges from (0, 1), 1.0 stands for perfect match, 0 stands for random labels.
+        dice matrix: array, ranges from (0, 1), shape = (label_number1, label_number2).
+
+    Notes
+    -----
+        1. the max label number in parcel should be assigned to the medial wall.
     """
-    pass
+    row_num = np.max(parcel1)
+    column_num = np.max(parcel2)
+    dice_matrix = np.zeros((row_num, column_num))
+    for i in range(row_num):
+        for j in range(column_num):
+            dice_matrix[i, j] = calc_overlap(parcel1, parcel2, i, j)
+    return dice_matrix
 
 
-def silhouette_coef(parcel1, parcel2):
+def dice_coef(parcel1, parcel2):
+    """
+    Calculate mean dice coefficient of parcel1 and parcel2.
+
+    Parameters
+    ----------
+        parcel1: cluster labels, shape = [n_samples].
+        parcel2: cluster labels, shape = [n_samples].
+
+    Returns
+    -------
+        dice coef: float, ranges from (0.0, 1.0).
+
+    Notes
+    -----
+        1. the max label number in parcel should be assigned to the medial wall.
+    """
+    return np.mean(dice_mat(parcel1, parcel2))
+
+
+def silhouette_coef(data, parcel):
     """
     Calculate silhouette coefficient of parcel1 and parcel2.
 
     Parameters
     ----------
-        parcel1: cluster labels, shape = [n_samples].
-        parcel2: cluster labels, shape = [n_samples].
+        data: time series, shape = [n_samples, n_features].
+        parcel: cluster labels, shape = [n_samples].
 
     Returns
     -------
         silhouette coefficient
     """
-    pass
+    return silhouette_score(data, parcel)
