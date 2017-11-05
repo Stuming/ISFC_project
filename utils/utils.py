5d3ecc4d93d574e57ad1d44eb38b9ef3ad65d6c5
Stuming <1361046649@qq.com>
2017-11-05 22:18:04 +0800

    *BF**NF*fix a bug in '_reorder' to avoid change label of medial wall, and add return number of same label in 'relabel'

--- utils/utils.py
+++ utils/utils.py
@@ -160,7 +160,7 @@ def get_label_contour(labels, faces, contour_label=None):
     return labels
 
 
-def relabel(labels1, labels2, reorder=False):
+def relabel(labels1, labels2, reorder=False, return_matched_number=False):
     """
     Relabel labels1 and labels2 to make two most overlapped labels have the same label number,
       new_label_number = parcel_num + label_number of labels1,
@@ -179,11 +179,15 @@ def relabel(labels1, labels2, reorder=False):
                  label number before display.
                  Reorder makes label number grow from 0 if two labels are same, otherwise
                  reduces from 'parcel_num - 1'.
+        return_matched_number: return marched number for other analysis, default is False.
 
     Returns
     -------
         relabels1: cluster labels after relabeling, shape = [n_samples].
+                   if 'reorder=True', return labels after reordering.
         relabels2: cluster labels after relabeling, shape = [n_samples].
+                   if 'reorder=True', return labels after reordering.
+        matched_number: if 'return_matched_number=True', return number of matched labels.
 
     Notes
     -----
@@ -191,6 +195,7 @@ def relabel(labels1, labels2, reorder=False):
     """
     dice_mat = dice_matrix(labels1, labels2)
     parcel_num = int(np.max(labels1))
+    matched_number = 0
 
     # if dice_mat[i,j]==dice_mat[j,i], this two labels([i,j]) would have same label number.
     for i in range(parcel_num):
@@ -200,10 +205,17 @@ def relabel(labels1, labels2, reorder=False):
             print("Relabel %i & %i into %i" % (i, j, parcel_num + i))
             labels1[np.where(labels1 == i)] = parcel_num + 1 + i
             labels2[np.where(labels2 == j)] = parcel_num + 1 + i
+            matched_number = matched_number + 1
     if reorder:
         labels1_ro = _reorder(labels1, parcel_num)
         labels2_ro = _reorder(labels2, parcel_num)
+
+        if return_matched_number:
+            return labels1_ro, labels2_ro, matched_number
         return labels1_ro, labels2_ro
+
+    if return_matched_number:
+        return labels1, labels1, matched_number
     return labels1, labels1
 
 
@@ -235,9 +247,13 @@ def _reorder(labels, parcel_num):
         if label >= parcel_num + 1:
             labels_ro[np.where(labels == label)] = i
             i = i + 1
-        else:
+        elif label < parcel_num:
             labels_ro[np.where(labels == label)] = parcel_num - 1 - j
             j = j + 1
+        elif label == parcel_num:  # parcel number of medial wall
+            continue
+        else:
+            raise ValueError("Please check value of input 'labels'.")
     print("Number of matched label: %i" % i)
     print("Number of unmatched label: %i" % j)
     return labels_ro
